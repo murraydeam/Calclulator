d967c2aee349fc13a3c9a7426f81a7111bbb2456
deand <Deandremurray22@gmail.com>
2022-03-21 14:37:46 -0400

    Delete Button connected

--- frontend/src/components/Calculator.jsx
+++ frontend/src/components/Calculator.jsx
@@ -14,6 +14,13 @@ export const ACTIONS = {
 export function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state, 
+        currentOperand: payload.digit,
+        overwrite: false
+        }
+      }
       if (payload.digit === "0" && state.currentOperand === "0") return state;
       if (payload.digit === "." && state.currentOperand.includes("."))
         return state;
@@ -23,6 +30,22 @@ export function reducer(state, { type, payload }) {
       };
     case ACTIONS.CLEAR:
       return {};
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null }
+      }
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      }
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
@@ -34,6 +57,7 @@ export function reducer(state, { type, payload }) {
 
       return {
         ...state,
+        overwrite: true,
         previousOperand: null, 
         operation: null,
         currentOperand: evaluate(state),
@@ -108,7 +132,7 @@ export const Calculator = () => {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
