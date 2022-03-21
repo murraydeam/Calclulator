97080043645bcd97df05a840b04ab813ce9d42ea
deand <Deandremurray22@gmail.com>
2022-03-21 13:49:31 -0400

    first commit

---
+++ frontend/src/components/Calculator.jsx
@@ -0,0 +1,57 @@
+import { useReducer } from "react";
+import DigitButton from "./DigitButton";
+import "./styles.css";
+
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+export function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
+      };
+  }
+}
+
+export const Calculator = () => {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+  return (
+    // The entire Calculator
+    <div className="calculator-grid">
+      {/* Display */}
+      <div className="output">
+        <div className="previous-operand">{currentOperand} {operation}</div>
+        <div className="current-operand">{previousOperand}</div>
+      </div>
+      {/* Buttons */}
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <DigitButton digit="1" dispatch={dispatch} />
+      <DigitButton digit="2" dispatch={dispatch} />
+      <DigitButton digit="3" dispatch={dispatch} />
+      <DigitButton digit="4" dispatch={dispatch} />
+      <DigitButton digit="5" dispatch={dispatch} />
+      <DigitButton digit="6" dispatch={dispatch} />
+      <DigitButton digit="7" dispatch={dispatch} />
+      <DigitButton digit="8" dispatch={dispatch} />
+      <DigitButton digit="9" dispatch={dispatch} />
+      <DigitButton digit="0" dispatch={dispatch} />
+      <button>-</button>
+      <button>.</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+};
+
+export default Calculator;
